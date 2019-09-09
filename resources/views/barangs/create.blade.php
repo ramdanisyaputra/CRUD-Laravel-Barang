8aed24cf921e991b610c94ad8a2ee874ce90b808
Your Name <rmdn.syaputra@gmail.com>
2019-09-09 15:43:19 +0700

    first commit

---
+++ resources/views/barangs/create.blade.php
@@ -0,0 +1,60 @@
+@extends('barangs.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Tambah Barang</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('barangs.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('barangs.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Harga</strong>
+                <input type="number" name="harga" class="form-control" placeholder="Harga">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kategori</strong>
+                <input type="text" name="kategori" class="form-control" placeholder="Kategori">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Stok</strong>
+                <input type="number" name="stok" class="form-control" placeholder="Stok">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ resources/views/barangs/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('barangs.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Barang</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('barangs.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('barangs.update',$barang->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name</strong>
+                    <input type="text" name="name" value="{{ $barang->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Harga</strong>
+                    <input type="number" name="harga" value="{{ $barang->harga }}" class="form-control" placeholder="Harga">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Kategori</strong>
+                    <input type="text" name="kategori" value="{{ $barang->kategori }}" class="form-control" placeholder="Kategori">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Stok</strong>
+                    <input type="number" name="stok" value="{{ $barang->stok }}" class="form-control" placeholder="Stok">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
